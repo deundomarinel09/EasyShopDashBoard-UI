dc4071671f9df187723865085764ca33b0f935d8
John Roe Dannug <jr.dannug@ipay88.com.ph>
2025-05-13 23:40:55 +0800

    adjustment for admin dashboard

--- src/pages/products/AddCategory.tsx
+++ src/pages/products/AddCategory.tsx
@@ -93,7 +93,7 @@ const AddCategory = () => {
               value={categoryName}
               onChange={(e) => setCategoryName(e.target.value)}
               className="mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 focus:ring-blue-500 focus:border-blue-500"
-              placeholder="e.g. Electronics"
+              placeholder="e.g. Condiments"
             />
           </div>
 
